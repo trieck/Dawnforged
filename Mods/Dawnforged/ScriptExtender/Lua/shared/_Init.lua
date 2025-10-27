747d03ea741bacd1c4c6358cc5b3d17e533e0f14
Thomas A. Rieck <trieck@gmail.com>
2025-10-27 08:42:25 -0400

    Formatting.

--- Mods/Dawnforged/ScriptExtender/Lua/shared/_Init.lua
+++ Mods/Dawnforged/ScriptExtender/Lua/shared/_Init.lua
@@ -29,25 +29,25 @@ function LevelGamePlayReady(_LevelName, _IsEditorMode)
     local chest = "a0a59732-eb0c-489c-a8cb-488ab858de28" -- GustavDev/TUT_Avernus_C/Items/S_Chest_Secret_Grenades
 
     local DawnforgedItems = {
-        ShadowHeart            = "2f5636ff-8f50-47cd-9e3e-267ee39e806b",
-        Argentum               = "4afeda45-5957-410c-b13c-e37e0824d7ed",
-        Tonitrum               = "b9a39f76-73a8-4d6d-8f83-d93d3e0d015c",
-        Patentibus             = "1638043b-c566-41ac-8d36-a2aaedb7e32c",
-        Tonitrolus             = "d63151b1-4cc1-45d6-948e-60b3b620058a",
-        Malevolence            = "2d0a3da2-d188-425e-9a55-77e2035f713b",
-        Malice                 = "8dd8e38c-43fe-4b18-a41e-b806e78c3f62",
-        VerdantFang            = "ca6f5186-f56b-492e-8720-fbcab7a38b2c",
-        Moonshadow             = "1186205f-5247-4015-824b-87b6ccf3aeb7",
-        WarpBinder             = "91d269b6-6b9d-4c24-a107-50e7765c1117",
-        GravebindersMantle     = "a997b51b-2f68-47da-9970-ef7eca06e4e2",
-        Furybound              = "c221a696-d25e-47b7-816e-36889eed00a6",
-        Moonfinger             = "7a72adc8-e6e7-46d5-b8b8-be2f2ce9b51d",
-        Lockbane               = "fa962a4f-2c7a-4d47-8b2c-1878e84a9236",
-        Dawnstrider            = "65fe45e8-2144-42f8-a181-0cce9c28a779",
-        MorninglordsMantle     = "f3f9b424-a19d-4846-a06c-a35ac2b4f353",
-        RingSilverThreshold    = "9c8268ba-f57e-423d-a0cd-0576254dc691",
-        DawnforgedHammer       = "315500b0-02d4-4827-a021-bc418e97ffc1",
-        Sunpiercer             = "211bc74f-29d8-4d29-89fe-21ac1d5b8ee6"
+        ShadowHeart = "2f5636ff-8f50-47cd-9e3e-267ee39e806b",
+        Argentum = "4afeda45-5957-410c-b13c-e37e0824d7ed",
+        Tonitrum = "b9a39f76-73a8-4d6d-8f83-d93d3e0d015c",
+        Patentibus = "1638043b-c566-41ac-8d36-a2aaedb7e32c",
+        Tonitrolus = "d63151b1-4cc1-45d6-948e-60b3b620058a",
+        Malevolence = "2d0a3da2-d188-425e-9a55-77e2035f713b",
+        Malice = "8dd8e38c-43fe-4b18-a41e-b806e78c3f62",
+        VerdantFang = "ca6f5186-f56b-492e-8720-fbcab7a38b2c",
+        Moonshadow = "1186205f-5247-4015-824b-87b6ccf3aeb7",
+        WarpBinder = "91d269b6-6b9d-4c24-a107-50e7765c1117",
+        GravebindersMantle = "a997b51b-2f68-47da-9970-ef7eca06e4e2",
+        Furybound = "c221a696-d25e-47b7-816e-36889eed00a6",
+        Moonfinger = "7a72adc8-e6e7-46d5-b8b8-be2f2ce9b51d",
+        Lockbane = "fa962a4f-2c7a-4d47-8b2c-1878e84a9236",
+        Dawnstrider = "65fe45e8-2144-42f8-a181-0cce9c28a779",
+        MorninglordsMantle = "f3f9b424-a19d-4846-a06c-a35ac2b4f353",
+        RingSilverThreshold = "9c8268ba-f57e-423d-a0cd-0576254dc691",
+        DawnforgedHammer = "315500b0-02d4-4827-a021-bc418e97ffc1",
+        Sunpiercer = "211bc74f-29d8-4d29-89fe-21ac1d5b8ee6"
     }
 
     for name, uuid in pairs(DawnforgedItems) do
@@ -62,7 +62,7 @@ function LevelGamePlayReady(_LevelName, _IsEditorMode)
 end
 
 Ext.Events.SessionLoaded:Subscribe(function()
-    _P("   [Dawnforged] Session loaded, registering LevelGamePlayReady listener.")    
+    _P("   [Dawnforged] Session loaded, registering LevelGamePlayReady listener.")
     Ext.Osiris.RegisterListener("LevelGamePlayReady", 2, "after", LevelGamePlayReady)
 end)
 
